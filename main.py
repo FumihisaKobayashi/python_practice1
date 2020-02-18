4fc089dcdf60285e1acd76a72e26b2c7219d6207
FumihisaKobayashi <ruruku717@gmial.com>
2020-02-18 23:48:16 +0900

    関数呼び出si

---
+++ main.py
@@ -0,0 +1,8 @@
+import sub
+
+a = 3
+
+b = 4
+
+c = sub.hiku(a,b)
+print(c)
---
+++ sub.py
@@ -0,0 +1,21 @@
+def plus(num1, num2):
+    """数字を足す。
+    
+    Arguments:
+        num1 {int} -- [description]
+        num2 {int} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    """
+    return num1 + num2
+
+
+def hiku(num1, num2):
+    return num1 - num2
+
+def waru(num1, num2):
+    return num1 / num2
+
+def kake(num1, num2):
+    return num1 * num2
